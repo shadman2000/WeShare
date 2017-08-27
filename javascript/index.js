0ab3ec36064f9aec6b006661415e00038fe2e8d8
Shadman Chowdhury <chowdhuryshadman0@gmail.com>
2017-08-27 07:41:13 -0700

    locations

--- javascript/index.js
+++ javascript/index.js
@@ -7,14 +7,36 @@ $(document).ready(function(){
 
 
 
+function getLocation(){
+    
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentLocation(showPosition);
+    }
+    else{
+        alert("Your Browser does not support geolocation. Please update your browser or try a different one");
+    }
+};
+
+function showPosition(positions){
+    var lat = positions.coords.latitude;
+    var long = positions.coords.longitude;
+    console.log(lat,long);
+}
+
+
 function initMap(){
-    var usa = {lat: 36.2116522, lng: 113.7155883};
+    
+    var usa = [[36.2116522, -113.7155883],[38.3679748,-105.2357827]];
     var map = new google.maps.Map(document.getElementById('googlemaps'), {
       zoom: 4,
-      center: usa
+      center: new google.maps.LatLng(36.2116522,-113.7155883)
     });
-    var marker = new google.maps.Marker({
-      position: usa,
-      map: map
-    });
-}
+    
+    for(var i = 0; i < usa.length ; i++){
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(usa[i][0],usa[i][1]),
+            map: map
+        });
+    }
+};
+
