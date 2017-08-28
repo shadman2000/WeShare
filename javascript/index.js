7d52a08cd87620f8d7736f166ef176f9750ac3a2
Shadman Chowdhury <chowdhuryshadman0@gmail.com>
2017-08-27 20:45:21 -0700

    mylocation Found!!

--- javascript/index.js
+++ javascript/index.js
@@ -12,30 +12,23 @@ $(document).ready(function(){
 
 function initMap(){
     
-    var usa = [[36.2116522, -113.7155883],[38.3679748,-105.2357827]];
     var map = new google.maps.Map(document.getElementById('googlemaps'), {
       zoom: 4,
       center: new google.maps.LatLng(36.2116522,-113.7155883)
     });
     
-    for(var i = 0; i < usa.length ; i++){
-        var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(usa[i][0],usa[i][1]),
-            map: map
-        });
-    };
-    
-    if (navigator.geolocation) {
+    if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-            var lat = position.coords.latitude;
-            var lng = position.coords.longitude;
-            map.seCenter(46.6190205,48.9108997);
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            map.setCenter(pos);
+            map.setMarker(new google.maps.Marker({
+                position:new google.maps.LatLng(pos.lat,pos.lng),
+                map:map
+            }));
         });
     }
-                                    
-    else{
-        alert("Your Browser does not support geolocation or please allow your browser to get your current location.");
-    }
-
-};
-
+}
